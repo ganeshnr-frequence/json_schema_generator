3e0677e81acde3c85bd555f8c9ebbe64f1bcc898
ganeshnr-frequence <ganesh.nr@frequence.com>
2023-02-20 21:19:00 +0530

    code refactored

--- generate_schema_for_widget.py
+++ generate_schema_for_widget.py
@@ -1,32 +1,34 @@
 import json
-from genson_ext import CustomSchemaBuilder
-from util import is_json
+from util import get_custom_builder, is_json_empty
 from db_executor import get_widget_config, get_strategy_widget_config
     
 #variables
 widget = get_widget_config()
 widget_config = get_strategy_widget_config()
 
+schema_count = 0
 
 for w in widget:
-    builder = CustomSchemaBuilder(schema_uri='http://json-schema.org/draft-07/schema#') # add json schema version
-       
-    if is_json(w['config_json']) and w['config_json'] != '{}' and w['config_json'] != '' and w['config_json'] != '[]':
+    builder = get_custom_builder()
+    schema_count += 1
+    
+    if is_json_empty(w['config_json']):
         builder.add_object(json.loads(w['config_json']))
-        
+    
+    # filder the config specific to the widget
     filtered_result = filter(lambda i: i['id'] == w['id'], widget_config)
+    
     for fr in filtered_result:
-        if is_json(fr['config_json']) and fr['config_json'] != '{}' and fr['config_json'] != '' and fr['config_json'] != '[]':
+        if is_json_empty(fr['config_json']):
                 builder.add_object(json.loads(fr['config_json']))
     
     
     # Serializing json
     json_object = json.dumps(builder.to_schema(), indent=4)    
     
-    # Writing to sample.json
     with open("./widget_schema/" + str(w['id']) + "." + w["name"] + ".schema.json", "w") as outfile:
         outfile.write(json_object)
     
-    
+print('Total No of Schemas Created: ' + str(schema_count))
 
 
--- util.py
+++ util.py
@@ -1,4 +1,5 @@
 import json
+from genson_ext import CustomSchemaBuilder
 
 # check if a string is json or not
 def is_json(json_string):
@@ -7,3 +8,11 @@ def is_json(json_string):
   except ValueError as e:
     return False
   return True
+
+# returns a new instance of the custom schema builder type
+def get_custom_builder():
+  return CustomSchemaBuilder(schema_uri='http://json-schema.org/draft-07/schema#')
+
+
+def is_json_empty(json_string):
+  return is_json(json_string) and json_string != '{}' and json_string != '' and json_string != '[]'
